e4a9219ce4b6118b37c9f5b4963d8087214dcd38
Dave Song <49602639+woony91@users.noreply.github.com>
2020-02-27 15:08:40 -0500

    finalized all the logic behind tic tac toe

--- app/src/main/java/com/example/tictactoeapp/MainActivity.java
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.java
@@ -2,18 +2,36 @@ package com.example.tictactoeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-// 0: celtics, 1: sixers
+
+// 0: celtics
+// 1: sixers
+// 2: empty
+
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 4, 8}, {2, 4, 6}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}};
     int activePlayer = 0;
+    boolean gameActive = true;
 
     public void dropIn (View view){
         ImageView counter = (ImageView) view;
-        Log.i("Tag", counter.getTag().toString());
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if (gameState[tappedCounter] == 2 && gameActive){
+
+        gameState[tappedCounter] = activePlayer;
+
         counter.setTranslationY(-1500);
 
         if (activePlayer == 0){
@@ -25,6 +43,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+        for (int[] winningPosition : winningPositions){
+            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2){
+                //Someone has won
+
+                gameActive = false;
+
+                String winner = "";
+                if (activePlayer == 1){
+                    winner = "Celtics";
+                } else {
+                    winner = "Sixers";
+                }
+
+                Button playAgainButton = (Button) findViewById(R.id.bt_playAgain);
+                TextView winnerTextView = (TextView) findViewById(R.id.tv_winner);
+
+                winnerTextView.setText(winner + " has won!");
+                playAgainButton.setVisibility(View.VISIBLE);
+                winnerTextView.setVisibility(View.VISIBLE);
+
+                }
+            }
+        }
+    }
+
+    public void playAgain (View view){
+        Button playAgainButton = (Button) findViewById(R.id.bt_playAgain);
+        TextView winnerTextView = (TextView) findViewById(R.id.tv_winner);
+        playAgainButton.setVisibility(View.INVISIBLE);
+        winnerTextView.setVisibility(View.INVISIBLE);
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++){
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+        }
+
+        for (int i = 0; i < gameState.length; i++){
+            gameState[i] = 2;
+        }
+
+        activePlayer = 0;
+        gameActive = true;
     }
 
     @Override
